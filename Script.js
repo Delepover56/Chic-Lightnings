03c679fae7df9a5c4cdb13ad408e35a3611dce77
Delepover56 <taha82426980@gmail.com>
2023-05-30 16:47:45 +0500

    first commit

---
+++ Script.js
@@ -0,0 +1,10 @@
+const  modal = document.querySelector("#modal")
+const  open = document.querySelector("#open")
+const  close = document.querySelector("#close")
+
+    open.addEventListener( 'click', ()=>{
+        modal.showModal()
+    })
+    close.addEventListener( 'click', () => {
+        modal.close()
+    })
